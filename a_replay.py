4b244ad1436a39c66725579a3387293eda67f0a1
cuixinyuan666 <1003888733@qq.com>
2026-04-25 02:00:44 +0800

    Initial import of a_chan_a

---
+++ a_replay.py
@@ -0,0 +1,8 @@
+from a_replay_parts.a_core import *
+from a_replay_parts.a_trainer import *
+from a_replay_parts.a_rld_backend import *
+from a_replay_parts.a_frontend import HTML
+from a_replay_parts.a_app import *
+
+if __name__ == "__main__":
+    run_dev_server()
